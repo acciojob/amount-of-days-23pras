0e489a7462f421886f06ebfdda5d269e97bc8d27
Prashant Pal <palprashant465@gmail.com>
2025-01-15 20:01:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  // Check if the year is a leap year
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366;  // Leap year
+  } else {
+    return 365;  // Regular year
+  }
+}
+
+
